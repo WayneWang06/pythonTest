49fdc5d36d6c651d8fa82764e34eebd44955b183
Wang <U6034148@ten.thomsonreuters.com>
2016-04-23 22:04:24 +0800

    add test case: ex1 - ex11.py

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Wayne Wang'
+my_age = 35
+my_height = 170
+my_weight = 190
+me_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'gray'
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eys and %s hair" %(me_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d and I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
---
+++ ex9.py
@@ -0,0 +1,22 @@
+# Here's some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three dobule-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+while False:
+	for i in ["/", "-", "|", "\\", "|"]:
+	#for i in ["0", "1", "2", "3", "4"]:
+		print "%s\r" % i,
+print '''
+It's magic
+'''		
